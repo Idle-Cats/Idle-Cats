3b19dfabc8bff035ba530fb35c8bd3877dace10d
AnnoyingTeemo <mjbwighton@gmail.com>
2022-08-08 17:49:19 +1200

    added test cat spawn

--- Assets/Scripts/Testing/BuildRoom.cs
+++ Assets/Scripts/Testing/BuildRoom.cs
@@ -10,6 +10,8 @@ public class BuildRoom : MonoBehaviour
 
     public float roomHeight;
 
+    public GameObject catTest;
+
     void Start()
     {
         roomHeight = testRoom.GetComponent<SpriteRenderer>().size.y;
@@ -18,6 +20,11 @@ public class BuildRoom : MonoBehaviour
     public void placeTestRoom() {
         GameObject room = Instantiate(testRoom, gameObject.GetComponent<BuildingNodePlacer>().node.transform.position, Quaternion.identity);
         room.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+
+        if (roomCount == 0) {
+            Instantiate(catTest, room.transform.position, Quaternion.identity);
+        }//remove this code later just for cat testing purposes
+
         roomCount++;
         gameObject.GetComponent<BuildingNodePlacer>().placeNode();
     }
