b1c686801d76749772448d6f65afa9c6d8619116
Michael7050 <michael7050@live.com>
2022-08-30 04:33:04 +1200

    Basic code for resourceroom/timerroom
    code for resourceroom and timer room mostly done, need help with plugging it into gui

--- Assets/Scripts/ResourceRoom.cs
+++ Assets/Scripts/ResourceRoom.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+wusing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,14 +7,51 @@ public class ResourceRoom : MonoBehaviour
     //looks good to me
     //skeleton class for resource rooms
     // Start is called before the first frame update
+
+    private float roomInvent = 0;
+    private double upgradeModifier = 0;
+    private int roomCapacity = 0;
+    private double resourceGen = 0;
+    private string name = "ResourceRoom";
+
     void Start()
     {
-        
+        //initialise values here
+        //temp placeholder code:
+        this.name = "Fishing Room";
+        this.roomCapacity = 100;
+        this.resourceGen = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //adds a tick of resource gen to roomInvent
+        if (roomInvent < roomCapacity)
+        {
+            addInvent(resourceGen * (1 + upgradeModifier));
+        }
+
+        //code for input?
+        //if (Input.ClickHere == true)
+        //{
+        //    globalResource = globalResource + roomInvent;
+        //    roomInvent = 0;
+        //}
         
+    }
+
+    //method for adding to invent making sure capacity isn't exceeded
+    void addInvent(int x)
+    {
+        int tempInvent = roomInvent + x;
+        if (tempInvent >= roomCapacity)
+        {
+            roomInvent = roomCapacity;
+        }
+        else
+        {
+            roomInvent = tempInvent;
+        }
     }
 }
--- Assets/Scripts/TimerRoom.cs
+++ Assets/Scripts/TimerRoom.cs
@@ -7,15 +7,33 @@ public class TimerRoom : MonoBehaviour
     //nathan is here
     //he says we suck
     //this is a skeleton class for timed rooms
+    private float timer = 0.0;
+    private boolean researching = false;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set up choices of rooms?
+        //set up various options?
+        //placeholder:
+        this.timer = 1000;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //have button
+        //if button pressed
+        //block out button, set researching to true
+        researching = true;
+        timer--;
+        if (timer <= 0)
+        {
+            researching = false;
+            //button for collect
+            //apply results
+        }
         
+
     }
 }
