e9e0d58d5a493a10f292de0dd7461920a3092948
ewok <mjbwighton@gmail.com>
2022-09-17 23:06:55 +1200

    made an old cat food artifact

---
+++ Assets/Scripts/Rooms/Artifact Rooms/ArtifactDisplay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ArtifactDisplay : MonoBehaviour
+{
+    public Artifact artifact;
+
+    public Image artworkImage;
+
+    public GameObject nameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        artworkImage.sprite = artifact.image;
+        nameText.GetComponent<TextMeshProUGUI>().SetText(artifact.name);
+    }
+}
