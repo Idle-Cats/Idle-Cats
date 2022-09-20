cbcdad5542927798f35a52017f08073a9e9ec6de
AnnoyingTeemo <mjbwighton@gmail.com>
2022-09-20 16:22:05 +1200

    rooms now take costs

--- Assets/Scripts/Rooms/CanAfford.cs
+++ Assets/Scripts/Rooms/CanAfford.cs
@@ -8,9 +8,23 @@ public class CanAfford : MonoBehaviour
     public PriceType priceType;
     public RoomSaveInfo.RoomType roomType;
 
+    public GameObject gameControl;
+
     public enum PriceType {
         Food,
         Minerals,
         Catpower
+    }
+
+    public void RemoveResources() {
+        if (priceType == PriceType.Minerals) {
+            gameControl.GetComponent<User>().minerals -= price;
+        }
+        else if (priceType == PriceType.Catpower) {
+            gameControl.GetComponent<User>().catPower -= price;
+        }
+        else if (priceType == PriceType.Food) {
+            gameControl.GetComponent<User>().food -= price;
+        }
     }
 }
