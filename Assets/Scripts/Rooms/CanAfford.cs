11b78ea782ee07d63c4417d60c691d4a549849d6
Michael7050 <93298842+Michael7050@users.noreply.github.com>
2022-09-22 14:30:14 +1200

    Sprint 1 deliverables done

--- Assets/Scripts/Rooms/CanAfford.cs
+++ Assets/Scripts/Rooms/CanAfford.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class CanAfford : MonoBehaviour
 {
-    public int price;
+    public int basePrice;
+    public int currentPrice;
     public PriceType priceType;
     public RoomSaveInfo.RoomType roomType;
 
     public GameObject gameControl;
+
+    void Start() {
+        currentPrice = basePrice;
+    }
 
     public enum PriceType {
         Food,
@@ -18,13 +23,13 @@ public class CanAfford : MonoBehaviour
 
     public void RemoveResources() {
         if (priceType == PriceType.Minerals) {
-            gameControl.GetComponent<User>().minerals -= price;
+            gameControl.GetComponent<User>().minerals -= currentPrice;
         }
         else if (priceType == PriceType.Catpower) {
-            gameControl.GetComponent<User>().catPower -= price;
+            gameControl.GetComponent<User>().catPower -= currentPrice;
         }
         else if (priceType == PriceType.Food) {
-            gameControl.GetComponent<User>().food -= price;
+            gameControl.GetComponent<User>().food -= currentPrice;
         }
     }
 }
