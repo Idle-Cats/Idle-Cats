401c60d02ba44d20355a87e372ad53faa0ef1d40
TheMysterys <the-mystery@outlook.com>
2022-10-11 21:27:03 +1300

    Changing background (base)
    Changes the background based on time of day on launch

---
+++ Assets/Scripts/UI/TimeOfDay.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimeOfDay : MonoBehaviour
+{
+
+    TimeSpan morning = new TimeSpan(6, 0, 0);
+    TimeSpan evening = new TimeSpan(18, 0, 0);
+    public Sprite day;
+    public Sprite night; 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeSpan now = DateTime.Now.TimeOfDay;
+        if (now > morning && now < evening)
+        {
+            GetComponent<SpriteRenderer>().sprite = day;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = night;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
