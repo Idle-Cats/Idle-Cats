44eb1422f957b678e65baaafe9a880ede96bb52b
AnnoyingTeemo <mjbwighton@gmail.com>
2022-10-21 13:55:55 +1300

    keanna and alexl done

--- Assets/Unit Tests/Tests/InternetTest.cs
+++ Assets/Unit Tests/Tests/InternetTest.cs
@@ -4,11 +4,11 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class UserTests
+public class InternetTest
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void UserTestsSimplePasses()
+    public void InternetTestSimplePasses()
     {
         // Use the Assert class to test conditions
     }
@@ -16,7 +16,7 @@ public class UserTests
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator UserTestsWithEnumeratorPasses()
+    public IEnumerator InternetTestWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
